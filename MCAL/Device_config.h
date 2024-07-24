9022e96ad6e8a323abd827fb4a5009c737d97a91
Ziad 7 <ziadelmekawy7@gmail.com>
2024-07-24 22:21:09 +0300

    MCAL layer

---
+++ MCAL/Device_config.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Device_config.h
+ * Author: hp
+ *
+ * Created on 14 ?????, 2023, 11:03 ?
+ */
+
+#ifndef DEVICE_CONFIG_H
+#define	DEVICE_CONFIG_H
+
+/* Section : includes */
+
+
+/* Section : Macro Declarations */
+
+/* Section : Macro Function Declarations */
+#define _XTAL_FREQ 4000000
+/* Section : Data Type Declarations */
+
+/* Section : Function Declarations */
+
+#endif	/* DEVICE_CONFIG_H */
+
